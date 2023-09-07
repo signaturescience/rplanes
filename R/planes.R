74878f79dfd6c34fb266f86264dfc152b2e5cf0b
VP Nagraj <nagraj@nagraj.net>
2023-09-07 11:14:28 -0400

    abbreviating plane_score example per devtools check NOTE

--- R/planes.R
+++ R/planes.R
@@ -417,8 +417,9 @@ plane_repeat <- function(location, input, seed, tolerance = NULL, prepend = NULL
 #' ## run plane scoring with select components
 #' plane_score(input = prepped_forecast, seed = prepped_seed, components = c("cover","taper"))
 #'
-#' ## run plane scoring with select components and additional args
-#' plane_score(input = prepped_forecast, seed = prepped_seed, components = c("cover", "trend", "repeats"), args = list(trend = list(sig_lvl = 0.05), repeats = list(prepend = 4, tolerance = 8)))
+#' ## run plane scoring with all components and additional args
+#' comp_args <- list(trend = list(sig_lvl = 0.05), repeats = list(prepend = 4, tolerance = 8))
+#' plane_score(input = prepped_forecast, seed = prepped_seed, args = comp_args)
 plane_score <- function(input, seed, components = "all", args = NULL) {
 
   ## TODO: create this list as a built-in object?
